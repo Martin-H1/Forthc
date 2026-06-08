f62526f960b258a99dbbed332ff0f5980cf9060c
Martin Heermance <mheermance@gmail.com>
2026-06-08 19:46:08 -0400

    Added more in depth testing of pictured I/O primitives.

--- tests/output.fs
+++ tests/output.fs
@@ -41,9 +41,21 @@
 ;
 
 : test-dots
- 1 2 3 4 .s cr ;
+ 1 2 3 4 .s cr clear ;
 
 : test-pic
+    ." 10 10 10 ud/mod (expect 0 1 1) = " 10 10 10 ud/mod .s cr clear
+    ." hld . (expect BSS address) = " hld .hex cr
+    ." pad-end <# hld @ - (expect 0) = " pad-end <# hld @ - .  cr
+    ." hld @ = " hld @ .hex cr
+    ." hld @ 50 hold hld @ - (expect 1) = " hld @ 50 hold hld @ - . cr
+    ." hld @ c@ (expect 50) = " hld @ c@ . cr
+    ." -31072 1 <# # (expect 10000 0) = " -31072 1 <# # .s cr clear
+    ." -31072 1 <# #s #> (expect addr 6) = " -31072 1 <# #s #> .s cr clear
+
+    ." -31072 1 d. (expect addr 6) = " -31072 1 d. cr clear
+
     1234 0 <# #s #> ." 1234 0 <# #s #> (expect 1234) = " type cr
     -1234 dup >r abs 0 <# #s r> sign #> ." (expect -1234) = " type cr
+    ." '" 8 0 10 d.r ." '" cr
 ;
